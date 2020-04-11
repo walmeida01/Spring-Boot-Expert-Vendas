cc1d581d5fe64580b55ebd51077cc0289c0a57a8
almei <williamdssa@gmail.com>
2020-04-11 18:14:11 -0300

    seção 3 - jpa(salvando e recuperando clientes)

--- src/main/java/com/walmeida/springexpert/VendasApplication.java
+++ src/main/java/com/walmeida/springexpert/VendasApplication.java
@@ -1,17 +1,36 @@
 package com.walmeida.springexpert;
 
+import com.walmeida.springexpert.domain.entity.Cliente;
+import com.walmeida.springexpert.domain.repository.Clientes;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @SpringBootApplication
 @RestController
 public class VendasApplication {
+
+    @Bean
+    public CommandLineRunner init(@Autowired Clientes clientes){
+        return  args -> {
+            clientes.salvar(new Cliente("William"));
+            clientes.salvar(new Cliente("Segundo Cliente"));
+
+            List<Cliente> todosClientes = clientes.obterTodos();
+            todosClientes.forEach(System.out::println);
+
+        };
+    }
 
     @Value("${application.name}")
     private String applicationName;
---
+++ src/main/java/com/walmeida/springexpert/domain/entity/Cliente.java
@@ -0,0 +1,48 @@
+package com.walmeida.springexpert.domain.entity;
+
+public class Cliente {
+
+    private Integer id;
+    private String nome;
+
+    public Cliente() {
+    }
+
+    public Cliente(Integer id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public Cliente(String nome) {
+        this.nome = nome;
+    }
+
+    public Cliente(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                '}';
+    }
+}
+
